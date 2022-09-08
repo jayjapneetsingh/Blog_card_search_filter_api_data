4f84f01d60b46b85866a830b5b78da2bf013630a
jay <jay.japneet@rubicotech.in>
2022-09-08 11:20:30 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+   <Blogs-Card  />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogsCard from '@/components/BlogsCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogsCard,
+    
   }
 }
 </script>
